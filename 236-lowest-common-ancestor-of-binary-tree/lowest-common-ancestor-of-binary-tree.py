b1e12f31f711a2b20e691d2218c963de85ba99be
AllenCoder <allencoder@126.com>
2019-01-20 17:04:01 +0800

    求x的阶乘

--- 236-lowest-common-ancestor-of-binary-tree/lowest-common-ancestor-of-binary-tree.py
+++ 236-lowest-common-ancestor-of-binary-tree/lowest-common-ancestor-of-binary-tree.py
@@ -25,3 +25,29 @@ class Solution(object):
             return root
 
         return l or r
+
+
+class TreeNode(object):
+    def __init__(self):
+        self.traverse_path = []
+        self.left = None
+        self.right = None
+        self.val = None
+
+    def preorder(self, root):
+        if root:
+            self.traverse_path.append(root.val)
+            self.preorder(root.left)
+            self.preorder(root.right)
+
+    def inorder(self, root):
+        if root:
+            self.inorder(root.left)
+            self.traverse_path.append(root.val)
+            self.inorder(root.right)
+
+    def postorder(self, root):
+        if root:
+            self.inorder(root.left)
+            self.inorder(root.right)
+            self.traverse_path.append(root.val)
